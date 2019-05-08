00373ffd239070964881a3213ffea7232b4736ed
NicoleLawson <nicolemlawson1994@gmail.com>
2019-05-08 14:16:23 -0500

    Lights flash (note updated their prefabs)

--- RUO (1)/Assets/LightScript.cs
+++ RUO (1)/Assets/LightScript.cs
@@ -4,19 +4,81 @@ using UnityEngine;
 
 public class LightScript : MonoBehaviour
 {
+    [SerializeField] private float flashTime = 0;
+   [SerializeField] private GameObject light;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        light = this.gameObject.transform.Find("Light").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //If the flash time is 0 it will start a countdown
+        if (flashTime == 0)
+        {
+            FlashLights();
+        }
     }
 
     //Method for breaking the lamp
     //To be called by explosions
+    public void BreakLights()
+    {
+        //play sound effect
+        
+        //turn off the light
+
+        // turn on broken glass
+
+    }
+
+
+    //Method for the lights to flash
+    public void FlashLights()
+    {
+        //Picks a number between 5 and 15 seconds
+        flashTime = Random.Range(5f, 15f);
+
+        //when it hits that point it flickers (on/off about 4 times)
+        StartCoroutine("StartFlash");
+    }
+
+    private IEnumerator StartFlash()
+    {
+        Debug.Log("StartFlash start");
+
+        yield return new WaitForSeconds(flashTime);
+
+        //first flash
+        light.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        light.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+
+        //second flash
+        light.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        light.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+
+        //third flash
+        light.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        light.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+
+        //fourth flash
+        light.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        light.SetActive(true);
+
+        Debug.Log("StartFlash end");
+
+        //Resets the flash time
+        flashTime = 0;
+    }
 
 }
